0f5279b343b42a4d30fa27e5ac2f900b625e9c7d
Miguel Angel Muñoz <thewintherdmc@gmail.com>
2023-04-30 15:57:32 -0500

    [Fix Layout]
    Fix layout in mobile

--- src/components/getHomeworks.js
+++ src/components/getHomeworks.js
@@ -127,15 +127,18 @@ const GetHomeworks = () => {
             </span>
             </button>
             {modifyFields && selectedTask && selectedTask.id === task.id && (
-          <div className="border rounded-md shadow mt-4 flex items-center px-4 h-20">
-            <input placeholder="new Name" onChange={(e) => setnewName(e.target.value)} className="border rounded-md shadow h-10 px-2 py-2 mr-2"/>
-            <input placeholder="new description" onChange={(e) => setNewDescription(e.target.value)} className="border rounded-md shadow h-10 px-2 py-2 mr-2"/>
-            <select placeholder="new priority" onChange={(e) => setNewPriority(e.target.value)} className="border rounded-md shadow h-10 px-2 py-2 mr-2" > 
+          <div className="md:border rounded-md shadow mt-4 flex items-center justify-center md:justify-start px-4 h-20">
+            <input placeholder="new Name" onChange={(e) => setnewName(e.target.value)} className="border rounded-md shadow md:h-10 w-20 md:w-36 px-2 py-2 mr-2"/>
+            <input placeholder="new description" onChange={(e) => setNewDescription(e.target.value)} className="border rounded-md shadow md:h-10 w-20 md:w-36 px-2 py-2 mr-2"/>
+            <select placeholder="new priority" onChange={(e) => setNewPriority(e.target.value)} className="border rounded-md shadow md:h-10 w-20 md:w-36 px-2 py-2 mr-2" > 
             <option value="">Choose Priority...</option>
             <option value="Low">Low</option>
             <option value="high">High</option>
             </select>
-            <button onClick={() => modify(task.id, newName, newDescription, newPriority)} className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Save</button>
+            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={() => modify(task.id, newName, newDescription, newPriority)}>
+            <svg aria-hidden="true" class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+            <span class="sr-only">Icon description</span>
+            </button>
           </div>
             )}
           </div>
